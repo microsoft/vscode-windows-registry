3bbc6dde6c4cf263e90052dbe6850d6d4a071532
Benjamin Pasero <benjpas@microsoft.com>
2019-08-05 11:52:40 +0200

    Convert to N-API (#3)
    * adopt n-api
    
    * add pipeline
    
    * Revert "add pipeline"
    
    This reverts commit 8aa58b8f2e62f24f0423cf6b5b84f790398f031a.
    
    * do not use  EINVAL in this case
    
    * chore: fix code style

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 const windowregistry = process.platform === 'win32' ? require('../build/Release/winregistry.node') : null;
 
 export type HKEY = "HKEY_CURRENT_USER" | "HKEY_LOCAL_MACHINE" | "HKEY_CLASSES_ROOT" | "HKEY_USERS" | "HKEY_CURRENT_CONFIG";
