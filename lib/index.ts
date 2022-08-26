1872fa2441ad35438541e6ec846b53fcff0a2dbf
Philipp Dunkel <pdunkel1@bloomberg.net>
2022-08-26 15:04:56 +0100

    add enumeration & writing; precompiled

--- lib/index.ts
+++ lib/index.ts
@@ -3,15 +3,54 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const windowregistry = process.platform === 'win32' ? require('../build/Release/winregistry.node') : null;
+const windowregistry = process.platform === 'win32' ? require('./winregistry.node') : null;
 
 export type HKEY = "HKEY_CURRENT_USER" | "HKEY_LOCAL_MACHINE" | "HKEY_CLASSES_ROOT" | "HKEY_USERS" | "HKEY_CURRENT_CONFIG";
+
+export function EnumRegKeyKeys(hive: HKEY, path: string) : string[] {
+	if (windowregistry) {
+		return windowregistry.EnumRegKeyKeys(hive, path);
+	} 
+	console.error('Could not initialize Windows Registry native node module.');
+	return [];
+}
+
+export function EnumRegKeyValues(hive: HKEY, path: string) : string[] {
+	if (windowregistry) {
+		return windowregistry.EnumRegKeyValues(hive, path);
+	} 
+	console.error('Could not initialize Windows Registry native node module.');
+	return [];
+}
 
 export function GetStringRegKey(hive: HKEY, path: string, name: string): string | undefined {
 	if (windowregistry) {
 		return windowregistry.GetStringRegKey(hive, path, name);
 	}
-
 	console.error('Could not initialize Windows Registry native node module.');
 	return undefined;
+}
+
+export function SetStringRegKey(hive: HKEY, path: string, name: string, value: string) : void {
+	if (windowregistry) {
+		windowregistry.SetStringRegKey(hive, path, name, value);
+	} else {
+		console.error('Could not initialize Windows Registry native node module.');
+	}
+}
+
+export function DeleteRegKeyKey(hive: HKEY, path: string, name: string) : void {
+	if (windowregistry) {
+		windowregistry.DeleteRegKeyKey(hive, path, name);
+	} else {
+		console.error('Could not initialize Windows Registry native node module.');
+	}
+}
+
+export function DeleteRegKeyValue(hive: HKEY, path: string, name: string) : void {
+	if (windowregistry) {
+		windowregistry.DeleteRegKeyValue(hive, path, name);
+	} else {
+		console.error('Could not initialize Windows Registry native node module.');
+	}
 }
--- test/windowsregistry.test.ts
+++ test/windowsregistry.test.ts
@@ -2,11 +2,55 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
-import { GetStringRegKey } from '../dist/index';
+import { randomUUID } from 'crypto';
+import { EnumRegKeyKeys, EnumRegKeyValues, GetStringRegKey, SetStringRegKey, DeleteRegKeyValue, DeleteRegKeyKey } from '../dist/index';
 import * as assert from 'assert';
 
 describe('Windows Registry Tests', () => {
+	describe('@EnumRegKeyKeys', ()=>{
+		it('List contains the CurrentVersion', ()=>{
+			const keys = EnumRegKeyKeys('HKEY_LOCAL_MACHINE', 'SOFTWARE\\Microsoft\\Windows');
+			assert(keys.includes('CurrentVersion'));
+		});
+
+		it('Validates argument count', () => {
+			assert.throws(() => (<any>EnumRegKeyKeys)());
+			assert.throws(() => ((<any>EnumRegKeyKeys)('HKEY_LOCAL_MACHINE')));
+		});
+
+		it('Validates argument length', () => {
+			let reallyLongString = 'areallystring';
+			while (reallyLongString.length < 17000) {
+				reallyLongString += reallyLongString;
+			}
+
+			assert.throws(() => ((<any>EnumRegKeyKeys)(reallyLongString, 'SOFTWARE\\Microsoft\\Windows')));
+			assert.throws(() => ((<any>EnumRegKeyKeys)('HKEY_LOCAL_MACHINE', reallyLongString, )));
+		});
+	});
+
+	describe('@EnumRegKeyValues', ()=>{
+		it('List contains the ProgramFilesPath', ()=>{
+			const values = EnumRegKeyValues('HKEY_LOCAL_MACHINE', 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion');
+			assert(values.includes('ProgramFilesPath'));
+		});
+
+		it('Validates argument count', () => {
+			assert.throws(() => (<any>EnumRegKeyValues)());
+			assert.throws(() => ((<any>EnumRegKeyValues)('HKEY_LOCAL_MACHINE')));
+		});
+
+		it('Validates argument length', () => {
+			let reallyLongString = 'areallystring';
+			while (reallyLongString.length < 17000) {
+				reallyLongString += reallyLongString;
+			}
+
+			assert.throws(() => ((<any>EnumRegKeyValues)(reallyLongString, 'SOFTWARE\\Microsoft\\Windows')));
+			assert.throws(() => ((<any>EnumRegKeyValues)('HKEY_LOCAL_MACHINE', reallyLongString, )));
+		});
+	});
+
 	describe('@GetStringRegKey', () => {
 		it('Retrieves the ProgramFilesPath registry value', () => {
 			const prgmFilesPath = GetStringRegKey('HKEY_LOCAL_MACHINE', 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion', 'ProgramFilesPath');
@@ -41,4 +85,40 @@ describe('Windows Registry Tests', () => {
 				reallyLongString)));
 		});
 	});
+
+	describe('Registry Writing', ()=>{
+		it('@SetStringRegKey', ()=>{
+			const uuid = randomUUID();
+			SetStringRegKey('HKEY_CURRENT_USER', 'NativeModuleTests\\vscode\\registry', 'value', uuid);
+			assert(uuid === GetStringRegKey('HKEY_CURRENT_USER', 'NativeModuleTests\\vscode\\registry', 'value'));
+		});
+		it('@DeleteRegKeyValue', ()=>{
+			const before = EnumRegKeyValues('HKEY_CURRENT_USER', 'NativeModuleTests\\vscode\\registry');
+			assert(before.includes('value'));
+			DeleteRegKeyValue('HKEY_CURRENT_USER', 'NativeModuleTests\\vscode\\registry', 'value');
+			const after = EnumRegKeyValues('HKEY_CURRENT_USER', 'NativeModuleTests\\vscode\\registry');
+			assert(!after.includes('value'));
+		});
+		it('@DeleteRegKeyKey(inner)', ()=>{
+			const before = EnumRegKeyKeys('HKEY_CURRENT_USER', 'NativeModuleTests\\vscode');
+			assert(before.includes('registry'));
+			DeleteRegKeyKey('HKEY_CURRENT_USER', 'NativeModuleTests\\vscode', 'registry');
+			const after = EnumRegKeyKeys('HKEY_CURRENT_USER', 'NativeModuleTests\\vscode');
+			assert(!after.includes('registry'));
+		});
+		it('@DeleteRegKeyKey(middle)', ()=>{
+			const before = EnumRegKeyKeys('HKEY_CURRENT_USER', 'NativeModuleTests');
+			assert(before.includes('vscode'));
+			DeleteRegKeyKey('HKEY_CURRENT_USER', 'NativeModuleTests', 'vscode');
+			const after = EnumRegKeyKeys('HKEY_CURRENT_USER', 'NativeModuleTests');
+			assert(!after.includes('vscode'));
+		});
+		it('@DeleteRegKeyKey(outer)', ()=>{
+			const before = EnumRegKeyKeys('HKEY_CURRENT_USER', '');
+			assert(before.includes('NativeModuleTests'));
+			DeleteRegKeyKey('HKEY_CURRENT_USER', '', 'NativeModuleTests');
+			const after = EnumRegKeyKeys('HKEY_CURRENT_USER', '');
+			assert(!after.includes('NativeModuleTests'));
+		});
+	})
 });
