21ce1b275dfec9740233f6b2f48cc0753d98c37e
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2023-06-08 07:53:33 -0700

    fix: allow types to be used on other platforms (#26)
    * Allow types to be used on other platforms
    * Bump CI
    * Add lockfile to package

--- lib/index.ts
+++ lib/index.ts
@@ -3,15 +3,14 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const windowregistry = process.platform === 'win32' ? require('../build/Release/winregistry.node') : null;
+const windowRegistry = process.platform === 'win32' ? require('../build/Release/winregistry.node') : null;
 
 export type HKEY = "HKEY_CURRENT_USER" | "HKEY_LOCAL_MACHINE" | "HKEY_CLASSES_ROOT" | "HKEY_USERS" | "HKEY_CURRENT_CONFIG";
 
 export function GetStringRegKey(hive: HKEY, path: string, name: string): string | undefined {
-	if (windowregistry) {
-		return windowregistry.GetStringRegKey(hive, path, name);
+	if (windowRegistry) {
+		return windowRegistry.GetStringRegKey(hive, path, name);
 	}
 
-	console.error('Could not initialize Windows Registry native node module.');
-	return undefined;
+	throw new Error('GetStringRegKey is only available on Windows.');
 }
